1a0db9c0df47035c98c00be056829bfcfbfe05ba
tkido <takanorikido@gmail.com>
2019-10-11 05:41:11 +0900

    update

--- lizard02.py
+++ lizard02.py
@@ -1,4 +1,26 @@
-from preamble import *
+#%%
+from IPython.display import set_matplotlib_formats, display
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import mglearn
+from cycler import cycler
+
+set_matplotlib_formats('pdf', 'png')
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['image.cmap'] = "viridis"
+plt.rcParams['image.interpolation'] = "none"
+plt.rcParams['savefig.bbox'] = "tight"
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['legend.numpoints'] = 1
+plt.rc('axes', prop_cycle=(
+    cycler('color', mglearn.plot_helpers.cm_cycle.colors) +
+    cycler('linestyle', ['-', '-', "--", (0, (3, 3)), (0, (1.5, 1.5))])))
+
+np.set_printoptions(precision=3, suppress=True)
+
+pd.set_option("display.max_columns", 8)
+pd.set_option('precision', 2)
 
 #%%
 
@@ -14,10 +36,46 @@ print("Target names: {}".format(iris_dataset['target_names']))
 print("Feature names: {}".format(iris_dataset['feature_names']))
 print("Type of data: {}".format(type(iris_dataset['data'])))
 print("Shape of data: {}".format(iris_dataset['data'].shape))
+print("First five columns of data:\n {}".format(iris_dataset['data'][:5]))
 
 
 #%%
-print("First five columns of data:\n {}".format(iris_dataset['data'][:5]))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dataset['data'], iris_dataset['target'], random_state=0
+)
+
+print("X_train shape: {}".format(X_train.shape))
+print("y_train shape: {}".format(y_train.shape))
+print("X_test shape: {}".format(X_test.shape))
+print("y_test shape: {}".format(y_test.shape))
+
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset['feature_names'])
+grr = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+
+
+#%%
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+
+#%%
+
+X_new = np.array([[5, 2.9, 1, 0.2]])
+print("X_new.shape: {}".format(X_new.shape))
+
+prediction = knn.predict(X_new)
+print("Predicted: {}".format(prediction))
+print("Predicted target name: {}".format(iris_dataset['target_names'][prediction]))
+#%%
+
+y_pred = knn.predict(X_test)
+print("Test set predictions: {}".format(y_pred))
+print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
 
 
 #%%

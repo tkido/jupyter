99f49f23876012922e6dee5d7285e55eef836332
tkido <tkido@xevo.com>
2019-10-10 17:32:01 +0900

    update

--- lizard02.py
+++ lizard02.py
@@ -4,9 +4,20 @@ from preamble import *
 
 from sklearn.datasets import load_iris
 iris_dataset = load_iris()
+
 print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
 print(iris_dataset['DESCR'])
+
+
+#%%
 print("Target names: {}".format(iris_dataset['target_names']))
 print("Feature names: {}".format(iris_dataset['feature_names']))
+print("Type of data: {}".format(type(iris_dataset['data'])))
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+
+
+#%%
+print("First five columns of data:\n {}".format(iris_dataset['data'][:5]))
+
 
 #%%
